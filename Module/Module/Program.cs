2e61a3745b8f2ebba2f1a9d90424437faad522b3
Sasha Make <sachka11231@gmail.com>
2024-10-29 09:23:09 +0200

    module

---
+++ Module/Module/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ConsoleAppDBBasics.Models;
+
+
+namespace Module
+{
+    public class ElectricityUsage
+    {
+        public int Id { get; set; } 
+        public string Month { get; set; } 
+        public double ConsumedKwh { get; set; } 
+        public double Rate { get; set; } 
+        public double Cost { get; set; } 
+    }
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<ElectricityUsage> ElectricityUsages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MSI-PC59\\TEMP;Database=ElectricityUsage:;Trusted_Connection=True;");
+        }
+    }
+    internal class Program
+    {
+        public static void CalculateTotalCost(params string[] months)
+        {
+            using (var context = new ApplicationContext())
+            {
+                var totalCost = context.ElectricityUsages
+                    .Where(e => months.Contains(e.Month))
+                    .Sum(e => e.Cost);
+
+                Console.WriteLine($"Total cost for month {string.Join(", ", months)}: {totalCost}");
+            }
+        }
+        public static void ExportToXml()
+        {
+            using (var context = new ApplicationContext())
+            {
+                var data = context.ElectricityUsages.ToList();
+                var dataTable = new DataTable("ElectricityUsage");
+
+                dataTable.Columns.Add("Id", typeof(int));
+                dataTable.Columns.Add("Month", typeof(string));
+                dataTable.Columns.Add("ConsumedKwh", typeof(double));
+                dataTable.Columns.Add("Rate", typeof(double));
+                dataTable.Columns.Add("Cost", typeof(double));
+
+                foreach (var usage in data)
+                {
+                    dataTable.Rows.Add(usage.Id, usage.Month, usage.ConsumedKwh, usage.Rate, usage.Cost);
+                }
+
+                var dataSet = new DataSet("ElectricityData");
+                dataSet.Tables.Add(dataTable);
+
+                string filePath = "ElectricityUsage.xml";
+                dataSet.WriteXml(filePath);
+                Console.WriteLine($"Savet in file {filePath}");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            // 1
+            Console.WriteLine("Enter months (comma separated, e.g., January,February):");
+            string[] months = Console.ReadLine()?.Split(',');
+            CalculateTotalCost(months);
+
+            // 2
+            ExportToXml();
+
+
+        }
+    }
+}
---
+++ Module/Module/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Module")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Module")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5a1da8bf-6036-4536-889b-0a7d12cee8b1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
